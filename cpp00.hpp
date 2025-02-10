3eb8904e2a88965277e990c94718e3cf00b72da4
Matheus Alves Barbosa Castro <malves-b@c1r8s3.42porto.com>
2025-02-10 20:11:51 +0000

    check

--- cpp00.hpp
+++ cpp00.hpp
@@ -6,7 +6,21 @@
 /*   By: malves-b <malves-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 19:55:46 by malves-b          #+#    #+#             */
-/*   Updated: 2025/01/28 16:18:43 by malves-b         ###   ########.fr       */
+/*   Updated: 2025/02/10 19:48:07 by malves-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+class Cachorro{
+	public:
+		Cachorro() : name(""), damage(5){}
+		int	cast_spell()
+		{
+			return damage;
+		}
+		std::string name;
+	private:
+		int damage;
+};
---
+++ srcs/ex02/ex.cpp
@@ -0,0 +1,11 @@
+#include "../../cpp00.hpp"
+
+using namespace std;
+
+int main(void)
+{
+	Cachorro Bruce;
+	Bruce.name = "cinho";
+	cout << Bruce.name << endl;
+	return 0;
+}
